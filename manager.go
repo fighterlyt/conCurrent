cafa23d2e9dc9fb3318f978d3ba9fdcf34945699
liuyt <liuyt@atomtree.cn>
2017-07-12 20:44:31 +0800

    添加上限管理

--- manager.go
+++ manager.go
@@ -3,41 +3,122 @@ package conCurrent
 import (
 	"context"
 	"fmt"
-	"sync/atomic"
+	"log"
+	"sync"
 )
+
+const (
+	defaultUpper        = 10000
+	WaitPolicy   Policy = iota
+	DiscardPolicy
+)
+
+var (
+	defaultPanicHandler = func(data interface{}) {
+		log.Println("捕捉到panic", data)
+	}
+)
+
+type Policy int
 
 type Manager struct {
 	count        int64  //并发数量
 	totalCount   uint64 //历史数量
-	panicHandler func()
+	upperLimit   int64  //并发上限
+	upperPolicy  Policy //数量触发上限后的策略
+	panicHandler func(interface{})
+	waitingList  List
+
+	*sync.Mutex
 }
 
 func (m *Manager) AddWork(work Work) {
-	m.start()
-	go func() {
-		defer func() {
-			if rcv := recover(); rcv != nil {
-				m.panicHandler()
-			}
-		}()
-		work(context.TODO())
-		m.finish()
-	}()
+	m.Lock()
+
+	if m.count >= m.upperLimit { //如果数量已经超过上限
+		m.processUpper(work)
+		m.Unlock()
+
+	} else {
+		m.start()
+		m.Unlock()
+		log.Println("释放锁")
+
+		go func(m *Manager, work Work) {
+			defer func() {
+				if rcv := recover(); rcv != nil {
+					m.panicHandler(rcv)
+					m.finish()
+				}
+
+			}()
+
+			work(context.TODO())
+
+			m.finish()
+
+		}(m, work)
+	}
 }
 
 func (m *Manager) start() {
-	atomic.AddInt64(&m.count, 1)
-	atomic.AddUint64(&m.totalCount, 1)
+	m.count++
+	m.totalCount++
+	m.show()
+
 }
 
 func (m *Manager) finish() {
-	atomic.AddInt64(&m.count, -1)
+	defer log.Println("完成")
+
+	m.Lock()
+	if work, ok := m.waitingList.Pop().(Work); ok {
+		log.Println("从队列中取", m.waitingList.Len())
+		m.totalCount++
+		m.Unlock()
+
+		defer func() {
+			if rcv := recover(); rcv != nil {
+				m.panicHandler(rcv)
+				m.finish()
+			}
+		}()
+		work(context.TODO())
+	} else {
+		m.Lock()
+		m.count--
+		m.show()
+
+		m.Unlock()
+
+	}
+
 }
 
 func NewManger() *Manager {
-	return &Manager{}
+	return &Manager{
+		upperLimit:   defaultUpper,
+		upperPolicy:  WaitPolicy,
+		panicHandler: defaultPanicHandler,
+		waitingList:  &PriorityQueue{},
+		Mutex:        &sync.Mutex{},
+	}
 }
 
 func (m Manager) show() {
-	fmt.Printf("%d/%d\n", m.count, m.totalCount)
+	fmt.Printf("%d/%d/%d\n", m.count, m.totalCount, m.waitingList.Len())
+}
+
+//processUpper 处理超过上限的并发
+func (m *Manager) processUpper(work Work) {
+	switch m.upperPolicy {
+	case WaitPolicy:
+		log.Println("放入队列")
+		m.waitingList.Push(&Item{
+			value:    work,
+			priority: 1,
+		})
+	case DiscardPolicy:
+		log.Println("达到上限")
+	}
 }
--- manager_test.go
+++ manager_test.go
@@ -9,6 +9,7 @@ import (
 
 var (
 	finish chan bool
+	count  int
 )
 
 func TestManager_AddWork(t *testing.T) {
@@ -26,6 +27,48 @@ func TestManager_AddWork(t *testing.T) {
 
 func println(ctx context.Context) {
 	fmt.Println("123")
-	time.Sleep(time.Second * 10)
+	time.Sleep(time.Second * 5)
 	finish <- true
+}
+
+func TestManager_PanicHandler(t *testing.T) {
+	m := NewManger()
+
+	m.show()
+	m.AddWork(panicPrintln)
+	m.show()
+
+	m.show()
+}
+
+func panicPrintln(ctx context.Context) {
+	fmt.Println("123")
+	time.Sleep(time.Second * 2)
+	panic("123")
+
+}
+
+func TestManager_AddWork2(t *testing.T) {
+	m := NewManger()
+	m.upperLimit = 5
+
+	for i := 0; i < 10; i++ {
+		m.AddWork(func(ctx context.Context) {
+			count++
+			fmt.Println(count)
+			time.Sleep(time.Second * 3)
+		})
+	}
+	//go func() {
+	//	for i := 0; i < 12; i++ {
+	//		m.show()
+	//		time.Sleep(time.Second)
+	//	}
+	//}()
+	time.Sleep(time.Second * 15)
+}
+
+func simple(ctx context.Context) {
+	fmt.Println("123")
+	time.Sleep(time.Second * 3)
 }
